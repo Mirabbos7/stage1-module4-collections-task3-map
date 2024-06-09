3c5589d1fdb4787a3b23423a9cf4765b4f6eb440
Mirabbos <mirabbosahmedov7777@gmail.com>
2024-06-09 23:51:19 +0500

    3 classes completed

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,16 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class KeyValueSwapper {
-    public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+    public HashMap<String, Integer> swap(Map<Integer, String> sourceMap) {
+        HashMap<String, Integer> swappedMap = new HashMap<>();
+        for (Map.Entry<Integer, String> entry : sourceMap.entrySet()) {
+            Integer key = entry.getKey();
+            String value = entry.getValue();
+            swappedMap.put(value, key);
+        }
+        return swappedMap;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,15 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        String[] lowerString = sentence.toLowerCase().split(" ");
+        Map<String, Integer> resMap = new HashMap<>();
+        for (String element : lowerString){
+            resMap.getOrDefault(element, 0);
+        }
+        return resMap;
     }
 }
